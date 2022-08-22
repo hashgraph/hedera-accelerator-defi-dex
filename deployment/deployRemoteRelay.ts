703fb80d511dc366ae430f02d1d1ea8748670ef9
Amandeep Singh <amandeep.rehal@iongroup.com>
2022-08-22 19:10:33 +0530

    Fixed the gas fee issue

--- deployment/deployRemoteRelay.ts
+++ deployment/deployRemoteRelay.ts
@@ -1,10 +1,16 @@
 
 import * as hre from "hardhat";
 import * as hethers from "@hashgraph/hethers";
+import { BigNumber } from "@ethersproject/bignumber";
+
 
  export const deployContract = async (contractName: string, contractConstructorArgs: any) => {
   const provider = new hre.ethers.providers.JsonRpcProvider("https://testnet.hashio.io/api");
   const wallet = new hre.ethers.Wallet("0x0f75e883f3963735905ba7324a4477cb774b9b7f08196743ad32831cb348fce3", provider);
+  wallet.estimateGas = async (tnx: any) => {
+    return BigNumber.from("500000");
+  }
+
   const constractFactory = await hre.ethers.getContractFactory(contractName, wallet);
 
   const contract = await constractFactory.deploy(contractConstructorArgs);
