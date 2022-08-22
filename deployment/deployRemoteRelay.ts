7c3b5135fc0c0e822e5eb67c0ecac15c1f671359
Amandeep Singh <amandeep.rehal@iongroup.com>
2022-08-22 17:03:20 +0530

    Made change for swap contract
    Signed-off-by: Amandeep Singh <amandeep.rehal@iongroup.com>

--- deployment/deployRemoteRelay.ts
+++ deployment/deployRemoteRelay.ts
@@ -2,22 +2,18 @@
 import * as hre from "hardhat";
 import * as hethers from "@hashgraph/hethers";
 
- const main = async () => {
+ export const deployContract = async (contractName: string, contractConstructorArgs: any) => {
   const provider = new hre.ethers.providers.JsonRpcProvider("https://testnet.hashio.io/api");
   const wallet = new hre.ethers.Wallet("0x0f75e883f3963735905ba7324a4477cb774b9b7f08196743ad32831cb348fce3", provider);
-  const BaseHTS = await hre.ethers.getContractFactory('BaseHTS', wallet);
+  const constractFactory = await hre.ethers.getContractFactory(contractName, wallet);
 
-  const baseHTS = await BaseHTS.deploy();
-  const contractAddress = (await baseHTS.deployTransaction.wait()).contractAddress;
+  const contract = await constractFactory.deploy(contractConstructorArgs);
+  const contractAddress = (await contract.deployTransaction.wait()).contractAddress;
   const contractId = hethers.utils.getAccountFromAddress(contractAddress);
 
-  console.log(`BaseHTS contract id: ${hethers.utils.asAccountString(contractId)}`);
-  console.log(`BaseHTS deployed to: ${contractAddress}`);
+  console.log(`${contractName} contract id: ${hethers.utils.asAccountString(contractId)}`);
+  console.log(`${contractName} deployed to: ${contractAddress}`);
 
   return contractAddress;
 };
 
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
