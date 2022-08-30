de419721e5a71e1efcf1c228c23e32edef33feb0
Amandeep Singh <amandeep.rehal@iongroup.com>
2022-08-30 19:02:35 +0530

    Read keys from properties files
    Signed-off-by: Amandeep Singh <amandeep.rehal@iongroup.com>

--- integrationTest/utils/utils.ts
+++ integrationTest/utils/utils.ts
@@ -3,13 +3,16 @@ import {
   PrivateKey,
   Client,
 } from "@hashgraph/sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default class ClientManagement {
-    private accountId = AccountId.fromString("0.0.47710057");
-    private accountKey = PrivateKey.fromString("3030020100300706052b8104000a04220420d38b0ed5f11f8985cd72c8e52c206b512541c6f301ddc9d18bd8b8b25a41a80f");
+    private accountId = AccountId.fromString(process.env.ADMIN_ID!);
+    private accountKey = PrivateKey.fromString(process.env.ADMIN_KEY!);
 
-    private treasure = AccountId.fromString("0.0.47645191");
-    private treasureKey = PrivateKey.fromString("308ed38983d9d20216d00371e174fe2d475dd32ac1450ffe2edfaab782b32fc5");
+    private treasure = AccountId.fromString(process.env.TREASURE_ID!);
+    private treasureKey = PrivateKey.fromString(process.env.TREASURE_KEY!);
 
     public createClientAsAdmin = (): Client => { 
         return this.doCreateClient(this.accountId, this.accountKey);
