959c84040396129ea64a8359f47bd19786c5ca89
Amandeep Singh <amandeep.rehal@iongroup.com>
2022-09-07 18:10:33 +0530

    Test transparent proxy

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,8 @@ npm --yes install --save-dev hardhat
 npm run codecoverage;
 msg=${COMMIT_MESSAGE:0:15};
 echo "msg " + $msg;
-if ["$msg" = "Deploy contract"]; then 
+
+if [ "$msg" = "Deploy contract"]; then 
     CONTRACT_TO_DEPLOY=${COMMIT_MESSAGE:16};
     echo "Running contract deployment ........ " + CONTRACT_TO_DEPLOY;
     contract=$CONTRACT_TO_DEPLOY.ts;
@@ -20,9 +21,12 @@ if ["$msg" = "Deploy contract"]; then
     npx hardhat run ./deployment/$contract;
     echo "Deployment done.";
 fi
+
+
 msg=${COMMIT_MESSAGE:0:33};
+
 echo "msg " + $msg;
-if ["$msg" = "Deploy transparent proxy contract"]; then 
+if [ "$msg" = "Deploy transparent proxy contract"]; then 
     CONTRACT_TO_DEPLOY=${COMMIT_MESSAGE:34};
     echo "Running proxy contract deployment ........ " + CONTRACT_TO_DEPLOY;
     echo OPERATOR_ID=$OPERATOR_ID >> .env;
