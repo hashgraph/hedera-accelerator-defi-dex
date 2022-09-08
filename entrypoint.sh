55f286d0a5b303a19b92da02f59136a83a033108
Amandeep Singh <amandeep.rehal@iongroup.com>
2022-09-08 17:50:57 +0530

    Deploy contract swap

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,53 @@
 #!/bin/sh
-# Change to the correct directory
 
 # Keep node alive
 set -e
 echo "Display context "
 echo $1
+COMMIT_MESSAGE=$1;
+OPERATOR_ID=$2;
+OPERATOR_KEY=$3;
+ADMIN_ID=$2;
+ADMIN_KEY=$3;
+
+npm config set strict-ssl false --global;
+export NODE_TLS_REJECT_UNAUTHORIZED='0';
+npm --yes install --save-dev hardhat
+# Run hardhat
+npm run codecoverage;
+msg=${COMMIT_MESSAGE:0:15};
+echo "msg " + $msg;
+
+if [ "$msg" = "Deploy contract" ]; then 
+    CONTRACT_TO_DEPLOY=${COMMIT_MESSAGE:16};
+    echo "Running contract deployment ........ " + CONTRACT_TO_DEPLOY;
+    echo OPERATOR_ID=$OPERATOR_ID >> .env;
+    echo OPERATOR_KEY=$OPERATOR_KEY >> .env;
+    echo ADMIN_ID=$ADMIN_ID >> .env;
+    echo ADMIN_KEY=$ADMIN_KEY >> .env;
+    cat .env;
+    contract=$CONTRACT_TO_DEPLOY.ts;
+    echo $contract;
+    npx hardhat run ./deployment/$contract;
+    echo "Deployment done.";
+fi
+
+msg=${COMMIT_MESSAGE:0:33};
+
+echo "msg " + $msg;
+if [ "$msg" = "Deploy transparent proxy contract" ]; then 
+    CONTRACT_TO_DEPLOY=${COMMIT_MESSAGE:34};
+    echo "Running proxy contract deployment ........ " + CONTRACT_TO_DEPLOY;
+    echo OPERATOR_ID=$OPERATOR_ID >> .env;
+    echo OPERATOR_KEY=$OPERATOR_KEY >> .env;
+    echo ADMIN_ID=$ADMIN_ID >> .env;
+    echo ADMIN_KEY=$ADMIN_KEY >> .env;
+    echo TREASURE_ID=$TREASURE_ID >> .env;
+    echo TREASURE_KEY=$TREASURE_KEY >> .env;
+    echo CONTRACT_ADDRESS=$CONTRACT_ADDRESS >> .env;
+    echo ADMIN_ADDRESS=$ADMIN_ADDRESS >> .env;
+    cat .env;
+    npx hardhat run ./deployment/transparentUpgradeableProxy.ts;
+    echo "Deployment done.";
+fi
 
