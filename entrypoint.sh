df09a10edb2168957ee663f8eb9a4c8c94edf584
Amandeep Singh <amandeep.rehal@iongroup.com>
2022-09-09 12:40:37 +0530

    Deploy contract swap

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,16 +12,16 @@ ADMIN_KEY=$3;
 TREASURE_ID=$4;
 TREASURE_KEY=$5;
 
-echo $COMMIT_MESSAGE
-echo $OPERATOR_ID
-echo $OPERATOR_KEY
-
-echo $TREASURE_ID
-echo $TREASURE_KEY
+echo COMMIT_MESSAGE $COMMIT_MESSAGE
+echo OPERATOR_ID $OPERATOR_ID
+echo TREASURE_ID $TREASURE_ID
 
 npm --yes install --save-dev hardhat
 # Run hardhat
 npm run codecoverage;
+
+msg=${COMMIT_MESSAGE:0:15};
+echo "msg for contract deployment " $msg;
 
 if [ "$msg" = "Deploy contract" ]; then 
     CONTRACT_TO_DEPLOY=${COMMIT_MESSAGE:16};
@@ -39,7 +39,7 @@ fi
 
 msg=${COMMIT_MESSAGE:0:33};
 
-echo "msg " + $msg;
+echo "msg for transparent proxy deployment " + $msg;
 if [ "$msg" = "Deploy transparent proxy contract" ]; then 
     CONTRACT_NAME=${COMMIT_MESSAGE:34};
     echo "Running proxy contract deployment ........ " + $CONTRACT_NAME;
