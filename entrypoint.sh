7f80b22e2da082b36d7e1bfec876384ff679958c
Amandeep Singh <amandeep.rehal@iongroup.com>
2022-09-12 12:57:36 +0530

    Fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 # Keep node alive
 set -e
-echo "Display context "
-echo $1
+echo "********************  Env variables *********************"
 COMMIT_MESSAGE=$1;
 OPERATOR_ID=$2;
 OPERATOR_KEY=$3;
@@ -16,12 +15,20 @@ echo COMMIT_MESSAGE $COMMIT_MESSAGE
 echo OPERATOR_ID $OPERATOR_ID
 echo TREASURE_ID $TREASURE_ID
 
+TAG_MESSAGE=$(git tag -l --format='%(subject)' | head -n1)
+
+COMMIT_MESSAGE=$TAG_MESSAGE
+
 npm --yes install --save-dev hardhat
+
+echo "********************  Running test and coverage *********************"
 
 npm run codecoverage;
 
+echo "********************  Deployment *************************************"
+
 msg=${COMMIT_MESSAGE:0:15};
-echo "message for contract deployment " $msg;
+echo "Message for contract deployment " $msg;
 
 if [ "$msg" = "Deploy contract" ]; then 
     CONTRACT_TO_DEPLOY=${COMMIT_MESSAGE:16};
@@ -30,19 +37,17 @@ if [ "$msg" = "Deploy contract" ]; then
     echo OPERATOR_KEY=$OPERATOR_KEY >> .env;
     echo ADMIN_ID=$ADMIN_ID >> .env;
     echo ADMIN_KEY=$ADMIN_KEY >> .env;
-    cat .env;
     contract=$CONTRACT_TO_DEPLOY.ts;
-    echo $contract;
     npx hardhat run ./deployment/$contract;
     echo "Deployment done.";
 fi
 
 msg=${COMMIT_MESSAGE:0:33};
+echo "Message for transparent proxy deployment " + $msg;
 
-echo "msg for transparent proxy deployment " + $msg;
 if [ "$msg" = "Deploy transparent proxy contract" ]; then 
     CONTRACT_NAME=${COMMIT_MESSAGE:34};
-    echo "Running proxy contract deployment ........ " + $CONTRACT_NAME;
+    echo "Running proxy deployment ........ " + $CONTRACT_NAME;
     echo OPERATOR_ID=$OPERATOR_ID >> .env;
     echo OPERATOR_KEY=$OPERATOR_KEY >> .env;
     echo ADMIN_ID=$ADMIN_ID >> .env;
@@ -50,15 +55,14 @@ if [ "$msg" = "Deploy transparent proxy contract" ]; then
     echo TREASURE_ID=$TREASURE_ID >> .env;
     echo TREASURE_KEY=$TREASURE_KEY >> .env;
     echo CONTRACT_NAME=$CONTRACT_NAME >> .env;
-    cat .env;
     npx hardhat run ./deployment/transparentUpgradeableProxy.ts;
-    echo "Deployment done.";
+    echo "Transparent proxy deployed.";
 fi
 
 
 msg=${COMMIT_MESSAGE:0:34};
+echo "Message for upgrade proxy logic contract " + $msg;
 
-echo "msg for upgrade proxy logic contract " + $msg;
 if [ "$msg" = "Upgrade transparent proxy contract" ]; then 
     CONTRACT_NAME=${COMMIT_MESSAGE:35};
     echo "Running upgrade logic contract ........ " + $CONTRACT_NAME;
@@ -69,7 +73,8 @@ if [ "$msg" = "Upgrade transparent proxy contract" ]; then
     echo TREASURE_ID=$TREASURE_ID >> .env;
     echo TREASURE_KEY=$TREASURE_KEY >> .env;
     echo CONTRACT_NAME=$CONTRACT_NAME >> .env;
-    cat .env;
     npx hardhat run ./deployment/upgradeProxy.ts;
     echo "Upgrade done.";
 fi
+
+echo "********************  Done *************************************"
