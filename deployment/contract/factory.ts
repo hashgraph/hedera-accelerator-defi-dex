f0d9c1b7c2225a5cd211bf7f54e5c6403f64dedc
akhilesh-singh-Lab49 <85480831+akhilesh-singh-Lab49@users.noreply.github.com>
2022-10-20 15:35:49 +0530

    factory contract deployment updated (#22)
    Signed-off-by: Akhilesh Singh <akhilesh.singh@iongroup.com>
    
    Signed-off-by: Akhilesh Singh <akhilesh.singh@iongroup.com>

--- deployment/contract/factory.ts
+++ deployment/contract/factory.ts
@@ -1,11 +1,14 @@
 import { Deployment } from "../deployContractOnTestnet";
+import {
+  ContractFunctionParameters
+} from "@hashgraph/sdk";
 
 const contractId = "0.0.48313084";
 
 async function main() {
     const deployment = new Deployment();
     const filePath = "./artifacts/contracts/Factory.sol/Factory.json";
-    const deployedContract = await deployment.deployContract(filePath, []);
+    const deployedContract = await deployment.deployContractAsClient(filePath, new ContractFunctionParameters());
     console.log("Factory deployed.");
 }
 
