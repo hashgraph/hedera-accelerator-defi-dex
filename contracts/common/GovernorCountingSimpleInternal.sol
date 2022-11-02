cd528658cdbb58a099b8f79c1db184359fac1127
Abhishek Sharma <abhishek.k.sharma@iongroup.com>
2022-11-02 15:24:17 +0530

    abstract governor created and token create governor added

--- contracts/common/GovernorCountingSimpleInternal.sol
+++ contracts/common/GovernorCountingSimpleInternal.sol
@@ -10,56 +10,15 @@ import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesQ
 import "./IERC20.sol";
 import "./IBaseHTS.sol";
 
-contract GovernorCountingSimpleInternal is
+abstract contract GovernorCountingSimpleInternal is
     Initializable,
     GovernorUpgradeable,
     GovernorSettingsUpgradeable,
     GovernorCountingSimpleUpgradeable,
     HederaTokenService
 {
-    using Bits for uint256;
     uint256 precision;
-
     IERC20 token;
-    address treasurer;
-    bytes treasurerKeyBytes;
-    address admin;
-    bytes adminKeyBytes;
-    string tokenName;
-    string tokenSymbol;
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(
-        IERC20 _token,
-        address _treasurer,
-        bytes memory _treasurerKeyBytes,
-        address _admin,
-        bytes memory _adminKeyBytes,
-        string memory _tokenName,
-        string memory _tokenSymbol,
-        uint256 _votingDelayValue,
-        uint256 _votingPeriodValue
-    ) public initializer {
-        token = _token;
-        precision = 10000000;
-        treasurer = _treasurer;
-        treasurerKeyBytes = _treasurerKeyBytes;
-        admin = _admin;
-        adminKeyBytes = _adminKeyBytes;
-        tokenName = _tokenName;
-        tokenSymbol = _tokenSymbol;
-        __Governor_init("HederaGovernor");
-        __GovernorSettings_init(
-            _votingDelayValue, /* 1 block */
-            _votingPeriodValue, /* 1 week */
-            0
-        );
-        __GovernorCountingSimple_init();
-    }
 
     function _getVotes(
         address account,
@@ -99,15 +58,6 @@ contract GovernorCountingSimpleInternal is
         return super.proposalThreshold();
     }
 
-    function quorum(uint256)
-        public
-        pure
-        override(IGovernorUpgradeable)
-        returns (uint256)
-    {
-        return 1;
-    }
-
     /**
      * @dev See {IGovernor-execute}.
      */
@@ -133,68 +83,6 @@ contract GovernorCountingSimpleInternal is
      */
     function voteSucceeded(uint256 proposalId) external view returns (bool) {
         return super._voteSucceeded(proposalId);
-    }
-
-    /**
-     * @dev Internal execution mechanism. Can be overridden to implement different execution mechanism
-     */
-    function _execute(
-        uint256, /* proposalId */
-        address[] memory,
-        uint256[] memory,
-        bytes[] memory,
-        bytes32 /*descriptionHash*/
-    ) internal virtual override {
-        // string memory errorMessage = "Governor: call reverted without message";
-        // for (uint256 i = 0; i < targets.length; ++i) {
-        //     (bool success, bytes memory returndata) = targets[i].call{value: values[i]}(calldatas[i]);
-        //     AddressUpgradeable.verifyCallResult(success, returndata, errorMessage);
-        // }
-        createToken();
-    }
-
-    function createToken()
-        internal
-        returns (int256 responseCode, address tokenAddress)
-    {
-        uint256 supplyKeyType;
-        uint256 adminKeyType;
-
-        IHederaTokenService.KeyValue memory supplyKeyValue;
-        supplyKeyType = supplyKeyType.setBit(4);
-        supplyKeyValue.ed25519 = treasurerKeyBytes;
-
-        IHederaTokenService.KeyValue memory adminKeyValue;
-        adminKeyType = adminKeyType.setBit(0);
-        adminKeyValue.ed25519 = adminKeyBytes;
-
-        IHederaTokenService.TokenKey[]
-            memory keys = new IHederaTokenService.TokenKey[](2);
-
-        keys[0] = IHederaTokenService.TokenKey(supplyKeyType, supplyKeyValue);
-        keys[1] = IHederaTokenService.TokenKey(adminKeyType, supplyKeyValue);
-
-        IHederaTokenService.Expiry memory expiry;
-        expiry.autoRenewAccount = treasurer;
-        expiry.autoRenewPeriod = 8000000;
-
-        IHederaTokenService.HederaToken memory newToken;
-        newToken.name = tokenName;
-        newToken.symbol = tokenSymbol;
-        newToken.treasury = treasurer;
-        newToken.expiry = expiry;
-        newToken.tokenKeys = keys;
-
-        (responseCode, tokenAddress) = createFungibleToken(
-            newToken,
-            uint256(0),
-            8
-        );
-
-        require(
-            responseCode == HederaResponseCodes.SUCCESS,
-            "Token creation failed"
-        );
     }
 }
 
