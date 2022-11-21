4bce997eee31cc89d7d26798743e66b907e88cdb
shivam_garg <shivam.garg@iongroup.com>
2022-11-21 13:07:00 +0530

    misc changes
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>

--- deployment/logicCode.ts
+++ deployment/logicCode.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const deployment = new Deployment();
 
-export async function main(_contractName: string | null = null) {
+export async function main(_contractName: string? = null) {
   const contractName = (
     _contractName ?? process.env.CONTRACT_NAME!
   ).toLowerCase();
