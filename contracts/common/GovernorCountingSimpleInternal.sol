80c30c392871c0fe2f6befa4e9cd8ea6402d4c63
Amandeep Singh <amandeep.rehal@iongroup.com>
2022-11-24 15:51:29 +0530

    Added delegate governor changes

--- contracts/common/GovernorCountingSimpleInternal.sol
+++ contracts/common/GovernorCountingSimpleInternal.sol
@@ -4,14 +4,12 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts-upgradeable/governance/GovernorUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/governance/IGovernorUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorSettingsUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorCountingSimpleUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesQuorumFractionUpgradeable.sol";
 import "./IERC20.sol";
 import "./IBaseHTS.sol";
 import "./hedera/HederaResponseCodes.sol";
-import "hardhat/console.sol";
- 
+import "./GovernorCountingSimpleUpgradeable.sol";
 
 abstract contract GovernorCountingSimpleInternal is
     Initializable,
@@ -25,17 +23,47 @@ abstract contract GovernorCountingSimpleInternal is
     mapping(uint256 => address) proposalCreators;
     IBaseHTS internal tokenService;
 
-    function mockFunctionCall() pure internal returns (
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas) {
-           targets = new address[](1);
-           targets[0] = (address(0));
-           values = new uint256[](1);
-           values[0] = (0);
-           calldatas = new bytes[](1);
-           bytes memory b = "blank";
-           calldatas[0] = (b);
+    function mockFunctionCall()
+        internal
+        pure
+        returns (
+            address[] memory targets,
+            uint256[] memory values,
+            bytes[] memory calldatas
+        )
+    {
+        targets = new address[](1);
+        targets[0] = (address(0));
+        values = new uint256[](1);
+        values[0] = (0);
+        calldatas = new bytes[](1);
+        bytes memory b = "blank";
+        calldatas[0] = (b);
+    }
+
+    mapping(address => VotingWeight) votingWeights;
+    address[] delegaters;
+
+    mapping(address => Delegation) delegation;
+
+    struct VotingWeight {
+        uint256 weight;
+        uint8 support;
+    }
+
+    struct Delegation {
+        address delegatee;
+        address delegator;
+    }
+
+    function delegateTo(address delegatee) external {
+        address delegator = _msgSender();
+        delegation[delegatee] = Delegation(delegatee, delegator);
+        delegaters.push(delegator);
+    }
+
+    function isDelegated(address account) private view returns (bool) {
+        return delegation[account].delegatee != address(0x0) ? true : false;
     }
 
     function _getVotes(
@@ -43,7 +71,11 @@ abstract contract GovernorCountingSimpleInternal is
         uint256,
         bytes memory /*params*/
     ) internal view virtual override returns (uint256) {
-        uint256 share = (token.balanceOf(account) * precision) /
+        address userAccount = account;
+        if (isDelegated(account)) {
+            userAccount = delegation[account].delegator;
+        }
+        uint256 share = (token.balanceOf(userAccount) * precision) /
             token.totalSupply();
         uint256 percentageShare = share / (precision / 100);
         return percentageShare;
@@ -54,16 +86,21 @@ abstract contract GovernorCountingSimpleInternal is
         uint256[] memory values,
         bytes[] memory calldatas,
         string memory description
-    ) public virtual override returns (uint256) { 
+    ) public virtual override returns (uint256) {
         getGODToken();
-        uint256 proposalId = super.propose(targets, values, calldatas, description);
+        uint256 proposalId = super.propose(
+            targets,
+            values,
+            calldatas,
+            description
+        );
         proposalCreators[proposalId] = msg.sender;
         return proposalId;
     }
 
     function getGODToken() internal {
         tokenService.associateTokenPublic(address(this), address(token));
-        int responseCode = tokenService.transferTokenPublic(
+        int256 responseCode = tokenService.transferTokenPublic(
             address(token),
             address(msg.sender),
             address(this),
@@ -75,7 +112,7 @@ abstract contract GovernorCountingSimpleInternal is
     }
 
     function returnGODToken(uint256 proposalId) internal {
-        int responseCode = tokenService.transferTokenPublic(
+        int256 responseCode = tokenService.transferTokenPublic(
             address(token),
             address(this),
             address(proposalCreators[proposalId]),
@@ -121,8 +158,14 @@ abstract contract GovernorCountingSimpleInternal is
     ) public returns (uint256 proposalId) {
         bytes32 descriptionHash = keccak256(bytes(description));
         proposalId = hashProposal(targets, values, calldatas, descriptionHash);
-        require(proposalCreators[proposalId] != address(0), "Proposal not found");
-        require(msg.sender == proposalCreators[proposalId], "Only proposer can cancel the proposal");
+        require(
+            proposalCreators[proposalId] != address(0),
+            "Proposal not found"
+        );
+        require(
+            msg.sender == proposalCreators[proposalId],
+            "Only proposer can cancel the proposal"
+        );
         proposalId = super._cancel(targets, values, calldatas, descriptionHash);
         returnGODToken(proposalId);
     }
@@ -165,6 +208,43 @@ abstract contract GovernorCountingSimpleInternal is
         bytes32 /*descriptionHash*/
     ) internal virtual override {
         returnGODToken(proposalId);
+    }
+
+    /**
+     * @dev See {IGovernor-castVote}.
+     */
+    function castVote(uint256 proposalId, uint8 support)
+        public
+        virtual
+        override
+        returns (uint256)
+    {
+        address voter = _msgSender();
+
+        uint256 weight = _castVote(proposalId, voter, support, "");
+
+        adjustVotingWeightIfAlreadyVoted(proposalId);
+
+        VotingWeight memory userWeight = VotingWeight(weight, support);
+        votingWeights[voter] = userWeight;
+
+        return weight;
+    }
+
+    function adjustVotingWeightIfAlreadyVoted(uint256 proposalId) private {
+        for (uint256 i = 0; i < delegaters.length; i++) {
+            address delegator = delegaters[i];
+            VotingWeight memory existingVotedWeight = votingWeights[delegator];
+            if (hasVoted(proposalId, delegator)) {
+                adjustVote(
+                    proposalId,
+                    delegator,
+                    existingVotedWeight.support,
+                    existingVotedWeight.weight,
+                    ""
+                );
+            }
+        }
     }
 }
 
--- test/governer-test.ts
+++ test/governer-test.ts
@@ -1,17 +1,20 @@
 // Commenting test as contract has dependency on HTS service which we need to mock somehow.
 import { expect } from "chai";
 import * as fs from "fs";
-import Web3 from "web3";
 
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { ethers, upgrades } from "hardhat";
-import { BigNumber, Overrides, PayableOverrides } from "ethers";
+import { Contract } from "ethers";
+import { ERC20Mock } from "../typechain";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("Governor Tests", function () {
   const zeroAddress = "0x1111111000000000000000000000000000000000";
-  let precision: BigNumber;
-  const fee = 1;
-  const web3 = Web3;
+  const precision = 100000000;
+  const total = 100 * precision;
+  const twentyPercent = (total * 0.2);
+  const thirtyPercent = (total * 0.3);
+  const fiftyPercent = (total * 0.5);
 
   const readFileContent = (filePath: string) => {
     const rawdata: any = fs.readFileSync(filePath);
@@ -31,9 +34,7 @@ describe("Governor Tests", function () {
       const treaKey = ethers.utils.toUtf8Bytes("treasurer public key");
       const adminKey = ethers.utils.toUtf8Bytes("Admin public key");
       const args = [zeroAddress, votingDelay, votingPeriod, zeroAddress];
-      const instance = await upgrades.deployProxy(Governor, args, {
-        unsafeAllow: ["delegatecall"],
-      });
+      const instance = await upgrades.deployProxy(Governor, args);
       await instance.deployed();
     });
   });
@@ -44,22 +45,21 @@ describe("Governor Tests", function () {
     const signers = await ethers.getSigners();
     mockBaseHTS.setFailType(0);
 
+    console.log(`\nsigners[0].address ${signers[0].address}`);
+    console.log(`signers[1].address ${signers[1].address}`);
+    console.log(`signers[2].address ${signers[2].address} \n`);
     const TokenCont = await ethers.getContractFactory("ERC20Mock");
-    const tokenCont = await TokenCont.deploy(1000000000, 1000000000);
+    const tokenCont = await TokenCont.deploy(total, 0);
+    await tokenCont.setUserBalance(signers[0].address, twentyPercent);
+    await tokenCont.setUserBalance(signers[1].address, thirtyPercent);
+    await tokenCont.setUserBalance(signers[2].address, fiftyPercent);
     const votingDelay = 0;
-    const votingPeriod = 1;
+    const votingPeriod = 12;
 
     const Governor = await ethers.getContractFactory("GovernorTokenCreate");
     console.log(`token Service address ${mockBaseHTS.address}`);
-    const args = [
-      tokenCont.address,
-      votingDelay,
-      votingPeriod,
-      mockBaseHTS.address,
-    ];
-    const instance = await upgrades.deployProxy(Governor, args, {
-      unsafeAllow: ["delegatecall"],
-    });
+    const args = [tokenCont.address, votingDelay, votingPeriod, mockBaseHTS.address];
+    const instance = await upgrades.deployProxy(Governor, args);
 
     await instance.deployed();
 
@@ -78,42 +78,31 @@ describe("Governor Tests", function () {
       return callData;
     };
 
-    it("getVotes for 100% shares", async function () {
-      const { instance, tokenCont } = await loadFixture(deployFixture);
-      const votes = await instance.getVotes(tokenCont.address, 1);
-      expect(votes).to.be.equals(100);
-    });
-
-    it("getVotes for 50% shares", async function () {
+    it("When user has 20% of token share then votes weight should be 20", async function () {
       const { instance, tokenCont, signers } = await loadFixture(deployFixture);
-      await tokenCont.setTotal(20);
-      await tokenCont.setUserBalance(signers[0].address, 10);
-      const votes = await instance.getVotes(tokenCont.address, 1);
-      expect(votes).to.be.equals(50);
+      await tokenCont.setTotal(100);
+      await tokenCont.setUserBalance(signers[0].address, 20);
+      const votes = await instance.connect(signers[0]).getVotes(tokenCont.address, 1);
+      expect(votes).to.be.equals(20);
     });
 
-    it("Test all states of proposal for cancel", async function () {
+    it("When user has 30% of token share then votes weight should be 30", async function () {
+      const { instance, tokenCont, signers } = await loadFixture(deployFixture);
+      await tokenCont.setTotal(100);
+      await tokenCont.setUserBalance(signers[1].address, 30);
+      const votes = await instance.getVotes(tokenCont.address, 1);
+      expect(votes).to.be.equals(30);
+    });
+
+    it("Verify proposal creation to cancel flow ", async function () {
       const { instance, tokenCont, signers } = await loadFixture(deployFixture);
       const treaKey = ethers.utils.toUtf8Bytes("treasurer public key");
       const adminKey = ethers.utils.toUtf8Bytes("Admin public key");
       const desc = "Test";
-      const userBalance = await tokenCont.balanceOf(signers[0].address);
-      expect(userBalance).to.be.equals(1000000000);
-      const proposalPublic = await instance
-        .connect(signers[0])
-        .createProposal(
-          desc,
-          zeroAddress,
-          treaKey,
-          zeroAddress,
-          adminKey,
-          "Token",
-          "Symbol"
-        );
-      const userBalanceAfterProposalCreation = await tokenCont.balanceOf(
-        signers[0].address
-      );
-      expect(userBalanceAfterProposalCreation).to.be.equals(900000000);
+
+      await verifyAccountBalance(tokenCont, signers[0].address, total * 0.2);
+      const proposalPublic = await instance.connect(signers[0]).createProposal(desc, zeroAddress, treaKey, zeroAddress, adminKey, "Token", "Symbol");
+      await verifyAccountBalance(tokenCont, signers[0].address, twentyPercent - (1 * precision));
 
       const record = await proposalPublic.wait();
       const proposalId = record.events[0].args.proposalId.toString();
@@ -121,7 +110,7 @@ describe("Governor Tests", function () {
       const delay = await instance.votingDelay();
       expect(delay).to.be.equals(0);
       const period = await instance.votingPeriod();
-      expect(period).to.be.equals(1);
+      expect(period).to.be.equals(12);
       const threshold = await instance.proposalThreshold();
       expect(threshold).to.be.equals(0);
       const quorumReached = await instance.quorumReached(proposalId);
@@ -135,39 +124,23 @@ describe("Governor Tests", function () {
       const quorumReached1 = await instance.quorumReached(proposalId);
       expect(quorumReached1).to.be.equals(true);
 
-      await mineNBlocks(2);
+      await mineNBlocks(20);
       const state = await instance.state(proposalId);
       expect(state).to.be.equals(4);
 
       await instance.cancel(desc);
-      const userBalanceAfterCancelProposal = await tokenCont.balanceOf(
-        signers[0].address
-      );
-      expect(userBalanceAfterCancelProposal).to.be.equals(1000000000);
+      await verifyAccountBalance(tokenCont, signers[0].address, twentyPercent);
     });
 
-    it("Test all states of proposal for execute", async function () {
+    it("Verify proposal creation to execute flow ", async function () {
       const { instance, tokenCont, signers } = await loadFixture(deployFixture);
       const treaKey = ethers.utils.toUtf8Bytes("treasurer public key");
       const adminKey = ethers.utils.toUtf8Bytes("Admin public key");
       const desc = "Test";
-      const userBalance = await tokenCont.balanceOf(signers[0].address);
-      expect(userBalance).to.be.equals(1000000000);
-      const proposalIdResponse = await instance
-        .connect(signers[0])
-        .createProposal(
-          desc,
-          zeroAddress,
-          treaKey,
-          zeroAddress,
-          adminKey,
-          "Token",
-          "Symbol"
-        );
-      const userBalanceAfterProposalCreation = await tokenCont.balanceOf(
-        signers[0].address
-      );
-      expect(userBalanceAfterProposalCreation).to.be.equals(900000000);
+
+      await verifyAccountBalance(tokenCont, signers[0].address, total * 0.2);
+      const proposalIdResponse = await instance.connect(signers[0]).createProposal(desc, zeroAddress, treaKey, zeroAddress, adminKey, "Token", "Symbol");
+      await verifyAccountBalance(tokenCont, signers[0].address, twentyPercent - (1 * precision));
 
       const record = await proposalIdResponse.wait();
       const proposalId = record.events[0].args.proposalId.toString();
@@ -175,7 +148,7 @@ describe("Governor Tests", function () {
       const delay = await instance.votingDelay();
       expect(delay).to.be.equals(0);
       const period = await instance.votingPeriod();
-      expect(period).to.be.equals(1);
+      expect(period).to.be.equals(12);
       const threshold = await instance.proposalThreshold();
       expect(threshold).to.be.equals(0);
       const quorumReached = await instance.quorumReached(proposalId);
@@ -189,15 +162,87 @@ describe("Governor Tests", function () {
       const quorumReached1 = await instance.quorumReached(proposalId);
       expect(quorumReached1).to.be.equals(true);
 
-      await mineNBlocks(2);
+      await mineNBlocks(20);
+      const state = await instance.state(proposalId);
+      expect(state).to.be.equals(4);
+
+      const call = await callParameters(tokenCont);
+      await instance.executePublic(call.targets, call.ethValues, call.calls, call.desc);
+      await verifyAccountBalance(tokenCont, signers[0].address, twentyPercent);
+    });
+
+    it("When user delegates votes to different account then delegator voting weight should be removed and delegatee votes should be considered ", async function () {
+      const { instance, tokenCont, signers } = await loadFixture(deployFixture);
+      const desc = "Test";
+      //Creating a proposal for 20% token share
+      await verifyAccountBalance(tokenCont, signers[0].address, twentyPercent);
+      const proposalIdResponse = await createProposal(tokenCont, instance, signers[0]);
+      await verifyAccountBalance(tokenCont, signers[0].address, twentyPercent - (1 * precision));
+
+      const record = await proposalIdResponse.wait();
+      const proposalId = record.events[0].args.proposalId.toString();
+
+      const delay = await instance.votingDelay();
+      expect(delay).to.be.equals(0);
+      const period = await instance.votingPeriod();
+      expect(period).to.be.equals(12);
+      const threshold = await instance.proposalThreshold();
+      expect(threshold).to.be.equals(0);
+      const quorumReached = await instance.quorumReached(proposalId);
+      expect(quorumReached).to.be.equals(false);
+      const voteSucceeded = await instance.voteSucceeded(proposalId);
+      expect(voteSucceeded).to.be.equals(false);
+      //Cast vote a for user 1.
+      await instance.castVote(proposalId, 0);
+      verifyProposalVotes(instance, signers[0], proposalId, { abstainVotes: 0, againstVotes: 0, forVotes: 19 })
+      //Cast another vote for user 2
+      await instance.connect(signers[1]).castVote(proposalId, 1);
+      verifyProposalVotes(instance, signers[1], proposalId, { abstainVotes: 0, againstVotes: 30, forVotes: 19 })
+      //user 1 delegates votes to user 3
+      await instance.delegateTo(signers[2].address);
+      //User 3 now votes with different choice
+      await instance.connect(signers[2]).castVote(proposalId, 1);
+      //User 1 votes share reduced to zero and user 3 votes adds to chosen option
+      verifyProposalVotes(instance, signers[1], proposalId, { abstainVotes: 0, againstVotes: 49, forVotes: 0 })
+
+      const voteSucceeded1 = await instance.voteSucceeded(proposalId);
+      expect(voteSucceeded1).to.be.equals(true);
+      const quorumReached1 = await instance.quorumReached(proposalId);
+      expect(quorumReached1).to.be.equals(true);
+
+      await mineNBlocks(20);
       const state = await instance.state(proposalId);
       expect(state).to.be.equals(4);
 
       await instance.executeProposal(desc);
-      const userBalanceAfterCancelProposal = await tokenCont.balanceOf(
-        signers[0].address
-      );
-      expect(userBalanceAfterCancelProposal).to.be.equals(1000000000);
+      await verifyAccountBalance(tokenCont, signers[0].address, twentyPercent);
     });
+
+    const callParameters = async (tokenCont: ERC20Mock) => {
+      return {
+        targets: [tokenCont.address],
+        ethValues: [0],
+        calls: [await getCallDataNew()],
+        desc: "Test"
+      }
+    };
+
+    const createProposal = async (tokenCont: ERC20Mock, instance: Contract, account: SignerWithAddress) => {
+      const call = await callParameters(tokenCont);
+      return await instance.connect(account).propose(call.targets, call.ethValues, call.calls, call.desc);
+    }
+
+    const verifyProposalVotes = async (instance: Contract, account: SignerWithAddress, proposalId: any, result: any) => {
+      const r = await instance.proposalVotes(proposalId);
+      expect(r.abstainVotes, "abstainVotes").to.be.equals(result.abstainVotes);
+      expect(r.againstVotes, "againstVotes").to.be.equals(result.againstVotes);
+      expect(r.forVotes, "forVotes").to.be.equals(result.forVotes);
+    }
+
+    const verifyAccountBalance = async (tokenCont: ERC20Mock, account: string, balance: number) => {
+      const userBalance = await tokenCont.balanceOf(account);
+      expect(userBalance, "Verify user balance ").to.be.equals(balance);
+    }
+
   });
 });
