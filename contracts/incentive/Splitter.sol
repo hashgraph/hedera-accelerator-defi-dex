ede8248c7e9484e953b7b7db3b0a8b35dcca72e8
Abhishek Sharma <abhishek.k.sharma@iongroup.com>
2022-12-21 12:31:07 +0530

    Solidity version updated

--- contracts/incentive/Splitter.sol
+++ contracts/incentive/Splitter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "../common/hedera/HederaResponseCodes.sol";
