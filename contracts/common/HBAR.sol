1f8b54e2944cd85a1e006b5c43103598c210b869
Abhishek Sharma <abhishek.k.sharma@iongroup.com>
2022-12-26 16:34:22 +0530

    removed HBAR

--- contracts/common/HBAR.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-interface HBAR {
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address account) external view returns (uint256);
-}
