14901b81b35752b4b957327524627b67f48970e4
IONTRADING\mandeep.singh <mandeep.singh@lab49.com>
2023-01-04 18:51:01 +0530

    exit once test completed

--- trigger-test.js
+++ trigger-test.js
@@ -3,46 +3,21 @@ const {
 } = require('child_process')
 
 // your cmd to start the process, possibly spawn('npm.cmd', ['test'))
-const proc = spawn('npm',['test'])
-let timer = null
+const proc = spawn('npm.cmd',['test'])
 proc.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`)
-  // look for the string in stdout
   if (data.includes('View your Cucumber Report at')) {
-    console.log('test run completed')   
-      setTimeout(() => {
-        proc.child.kill('SIGINT')
-      }, 1000)
-    
+    console.log('test run completed')
+    process.exit(0) 
+      
   }
 })
 
 proc.stderr.on('data', (data) => {
-  clearTimeout(timer)
   console.error(`stderr: ${data}`)
   if (data.includes('View your Cucumber Report at')) {  
     console.log('test run completed')      
-      setTimeout(() => {
-        proc.child.kill()
-      }, 1000)
+    process.exit(0);   
     
   }
 });
-
-proc.on('close', (code) => {
-  clearTimeout(timer)
-  console.log(`child process exited with code ${code}`);
-});
-
-process
-  .on('SIGTERM', shutdown('SIGTERM'))
-  .on('SIGINT', shutdown('SIGINT'))
-  .on('uncaughtException', shutdown('uncaughtException'))
-
-function shutdown(signal) {
-  return (err) => {
-    console.log(`\n${signal} signal received.`)
-    console.log('Killing child process.')
-    proc.child.kill()
-  }
-}
