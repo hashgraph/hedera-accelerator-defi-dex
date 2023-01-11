8583e7d1bd77f11fe50da83bfa95ba44668e645d
IONTRADING\mandeep.singh <mandeep.singh@lab49.com>
2023-01-11 17:28:45 +0530

    factory tests

--- e2e-test/step-definitions/FactoryTest.ts
+++ e2e-test/step-definitions/FactoryTest.ts
@@ -1,11 +1,9 @@
 import { binding, given, then, when } from "cucumber-tsflow";
-import { assert, expect } from "chai";
+import { expect } from "chai";
 import { ContractService } from "../../deployment/service/ContractService";
-import { DeployedContract } from "../../deployment/model/contract";
 import ClientManagement from "../../utils/ClientManagement";
 import { TokenId } from "@hashgraph/sdk";
 import Pair from "../business/Pair";
-import { BigNumber } from "bignumber.js";
 import Factory from "../business/Factory";
 
 const clientManagement = new ClientManagement();
@@ -27,6 +25,8 @@ let tokenOne: TokenId;
 let tokenTwo: TokenId;
 let expectedPairAddress: string;
 let actualPairAddress: string;
+let pairCountBefore: string[];
+let pairCountAfter: string[];
 
 @binding()
 export class FactorySteps {
@@ -63,7 +63,7 @@ export class FactorySteps {
   }
 
   @then(
-    /User verify address of pair of is same to address received after pair creation/,
+    /User verify address of pair is same to address received after pair creation/,
     undefined,
     30000
   )
@@ -75,5 +75,86 @@ export class FactorySteps {
       client
     );
     expect(actualPairAddress).to.eql(expectedPairAddress);
+  }
+
+  @when(/User get all pairs of tokens/, undefined, 30000)
+  public async getAllPairs(): Promise<void> {
+    pairCountBefore = await factory.getAllPairs(contractId, client);
+  }
+
+  @then(/User verifies count of pairs is increased by 1/, undefined, 30000)
+  public async verifyPairCount(): Promise<void> {
+    pairCountAfter = await factory.getAllPairs(contractId, client);
+    expect(pairCountAfter.length).to.eql(pairCountBefore.length + 1);
+  }
+
+  @when(/User create a new pair with same tokens/, undefined, 30000)
+  public async createPairWithSameTokens(): Promise<void> {
+    actualPairAddress = await factory.createPair(
+      contractId,
+      tokenOne,
+      tokenTwo,
+      treasureId,
+      treasureKey,
+      client
+    );
+  }
+
+  @when(/User create tokens with same name/, undefined, 30000)
+  public async createTokenWithSameName(): Promise<void> {
+    const num = Math.floor(Math.random() * 100) + 1;
+    tokenOne = await pair.createToken(
+      "FactoryTestSingleToken" + num,
+      treasureKey,
+      treasureId,
+      treasurerClient,
+      client
+    );
+    tokenTwo = await pair.createToken(
+      "FactoryTestSingleToken" + num,
+      treasureKey,
+      treasureId,
+      treasurerClient,
+      client
+    );
+  }
+
+  @then(/User verifies their address are different/, undefined, 30000)
+  public async verifyAddressOfPairWithSameToken(): Promise<void> {
+    expect(tokenOne.toSolidityAddress()).not.to.eql(
+      tokenTwo.toSolidityAddress()
+    );
+  }
+
+  @when(/User create a new token/, undefined, 30000)
+  public async createSingleToken(): Promise<void> {
+    const num = Math.floor(Math.random() * 100) + 1;
+    tokenOne = await pair.createToken(
+      "FactoryTestSingleToken" + num,
+      treasureKey,
+      treasureId,
+      treasurerClient,
+      client
+    );
+  }
+
+  @then(
+    /User gets message "([^"]*)" on creating pair with same token/,
+    undefined,
+    30000
+  )
+  public async userVerifyErrorMessage(msg: string): Promise<void> {
+    try {
+      await factory.createPair(
+        contractId,
+        tokenOne,
+        tokenOne,
+        treasureId,
+        treasureKey,
+        client
+      );
+    } catch (e: any) {
+      expect(e.message).contains(msg);
+    }
   }
 }
