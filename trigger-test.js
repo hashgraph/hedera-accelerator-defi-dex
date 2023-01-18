a8c609565224e728d6017cd304f020e7cedad631
IONTRADING\mandeep.singh <mandeep.singh@lab49.com>
2023-01-18 15:16:16 +0530

    fail job on test fail

--- trigger-test.js
+++ trigger-test.js
@@ -4,19 +4,29 @@ const {
 
 // your cmd to start the process, possibly spawn('npm.cmd', ['test'))
 const proc = spawn('npm',['test'])
+const flag = false;
 proc.stdout.on('data', (data) => {
+  if(data.includes('failed')){
+    flag = true;
+  }
   console.log(`stdout: ${data}`)
   if (data.includes('View your Cucumber Report at')) {
     console.log('test run completed')
-    setTimeout(() => {process.exit(0);}, 5000);  
-      
+    setTimeout(() => {process.exit(0);}, 5000);        
   }
+  
 })
 
 proc.stderr.on('data', (data) => {
   console.error(`stderr: ${data}`)
   if (data.includes('View your Cucumber Report at')) {
+    if(!flag){
     console.log('test run completed')
-    setTimeout(() => {process.exit(0);}, 5000);        
+    setTimeout(() => {process.exit(0);}, 5000);   
+    }
+    else{
+      console.log('few test cases failed')
+      setTimeout(() => {process.exit(1);}, 5000);
+    }     
   }
 });
