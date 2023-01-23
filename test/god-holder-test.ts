f494b5d3e78ce0d0c2957bbc47c80f19b87cd4b7
Abhishek Sharma <abhishek.k.sharma@iongroup.com>
2023-01-23 14:07:00 +0530

    GODHolder tests updated

--- test/god-holder-test.ts
+++ test/god-holder-test.ts
@@ -4,12 +4,12 @@ import * as fs from "fs";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { ethers, upgrades } from "hardhat";
 
-describe("Governor Tests", function () {
+describe("GODHolder Tests", function () {
   const zeroAddress = "0x1111111000000000000000000000000000000000";
   const precision = 100000000;
   const total = 100 * precision;
 
-  describe("GovernorCountingSimpleInternal Upgradeable", function () {
+  describe("GODHolder Upgradeable", function () {
     it("Verify if the GODHolder contract is upgradeable safe ", async function () {
       const Governor = await ethers.getContractFactory("GODHolder");
       const args = [zeroAddress, zeroAddress];
@@ -54,6 +54,34 @@ describe("Governor Tests", function () {
     await expect(
       godHolder.initialize(zeroAddress, zeroAddress)
     ).to.revertedWith("Initializable: contract is already initialized");
+  });
+
+  it("Verify GODHolder grabtoken pass", async function () {
+    const { godHolder, signers, mockBaseHTS, tokenCont } = await loadFixture(
+      deployFixture
+    );
+    const userTokens = await tokenCont.balanceOf(signers[0].address);
+    await godHolder.grabTokensFromUser(signers[0].address);
+    const contractTokens = await tokenCont.balanceOf(godHolder.address);
+    const userTokens1 = await tokenCont.balanceOf(signers[0].address);
+    expect(userTokens1).to.be.equal(0);
+    expect(userTokens).to.be.equal(contractTokens);
+  });
+
+  it("Verify Add and remove active proposals", async function () {
+    const { godHolder, signers, tokenCont } = await loadFixture(deployFixture);
+    await godHolder.addProposalForVoter(signers[0].address, 1);
+    await godHolder.addProposalForVoter(signers[1].address, 1);
+    const activeProposals = await godHolder.getActiveProposalsForUser();
+    expect(activeProposals.length).to.be.equal(1);
+    const canClaimGod = await godHolder.canUserClaimGodTokens();
+    expect(canClaimGod).to.be.equal(false);
+
+    await godHolder.removeActiveProposals([signers[0].address], 1);
+    const activeProposals1 = await godHolder.getActiveProposalsForUser();
+    expect(activeProposals1.length).to.be.equal(0);
+    const canClaimGod1 = await godHolder.canUserClaimGodTokens();
+    expect(canClaimGod1).to.be.equal(true);
   });
 
   it("Verify GODHolder grabtoken revert", async function () {
