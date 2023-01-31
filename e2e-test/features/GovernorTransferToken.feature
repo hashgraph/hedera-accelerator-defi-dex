952ba36f816cb78ccec5880b44e7e68b986945f4
IONTRADING\mandeep.singh <mandeep.singh@lab49.com>
2023-01-31 11:48:49 +0530

    governor transfer token

---
+++ e2e-test/features/GovernorTransferToken.feature
@@ -0,0 +1,10 @@
+@GovernorTransferToken
+Feature: GovernorTransferToken e2e test
+
+    This feature file contains e2e test for governor transfer token
+
+    Scenario: Verify user can create a proposal for transferring token
+    Given user have initialized the governor transfer token contract
+    When user create a new proposal with title "testtitle" description "testdescription" link "testlink" and token amount 3
+    Then user verify that recieved proposal id is not null
+
---
+++ e2e-test/step-definitions/GovernorTest.ts
@@ -0,0 +1,95 @@
+import { binding, given, then, when } from "cucumber-tsflow";
+import { expect } from "chai";
+import { ContractService } from "../../deployment/service/ContractService";
+import ClientManagement from "../../utils/ClientManagement";
+import { TokenId } from "@hashgraph/sdk";
+import dex from "../../deployment/model/dex";
+import Governor from "../business/Governor";
+import { BigNumber } from "bignumber.js";
+
+const governor = new Governor();
+
+const clientManagement = new ClientManagement();
+const contractService = new ContractService();
+
+const client = clientManagement.createOperatorClient();
+const { id } = clientManagement.getOperator();
+const { treasureId, treasureKey } = clientManagement.getTreasure();
+
+const contractId = contractService.getContractWithProxy(
+  contractService.governorTTContractName
+).transparentProxyId!;
+
+const adminClient = clientManagement.createClientAsAdmin();
+const { adminKey } = clientManagement.getAdmin();
+const htsServiceAddress = contractService.getContract(
+  contractService.baseContractName
+).address;
+const godHolder = contractService.getContract(
+  contractService.godHolderContract
+);
+
+let defaultQuorumThresholdValue: number = 5;
+let votingDelay: number = 0;
+let votingPeriod: number = 100;
+let proposalID: BigNumber;
+
+const transferTokenId = TokenId.fromString(dex.TOKEN_LAB49_1);
+
+@binding()
+export class GovernorSteps {
+  @given(
+    /user have initialized the governor transfer token contract/,
+    undefined,
+    30000
+  )
+  public async initialize(): Promise<void> {
+    console.log(
+      "*******************Starting governor transfer token test with following credentials*******************"
+    );
+    console.log("contractId : ", contractId);
+    console.log("TOKEN_USER_ID : ", id);
+    console.log("treasureId :", treasureId);
+    await governor.initialize(
+      contractId,
+      htsServiceAddress,
+      godHolder.transparentProxyAddress!,
+      defaultQuorumThresholdValue,
+      client,
+      votingDelay,
+      votingPeriod
+    );
+  }
+
+  @when(
+    /user create a new proposal with title "([^"]*)" description "([^"]*)" link "([^"]*)" and token amount (\d*)/,
+    undefined,
+    30000
+  )
+  public async createProposal(
+    title: string,
+    description: string,
+    link: string,
+    tokenAmount: number
+  ): Promise<void> {
+    let tokenQty = tokenAmount * 100000000;
+    let tokens = new BigNumber(tokenQty);
+    proposalID = await governor.propose(
+      contractId,
+      title,
+      description,
+      link,
+      id.toSolidityAddress(),
+      treasureId.toSolidityAddress(),
+      transferTokenId.toSolidityAddress(),
+      client,
+      treasureKey,
+      tokens
+    );
+  }
+
+  @then(/user verify that recieved proposal id is not null/, undefined, 30000)
+  public async verifyProposalID(): Promise<void> {
+    expect(proposalID).not.null;
+  }
+}
