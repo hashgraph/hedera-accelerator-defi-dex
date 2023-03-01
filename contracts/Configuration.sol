adb8fb6444ac32670fc70331cc52ff5be3715383
Amandeep Rehal <43944664+amandeeprehal-lab49@users.noreply.github.com>
2023-03-01 15:12:26 +0530

    Added url keys under configuration (#331)

--- contracts/Configuration.sol
+++ contracts/Configuration.sol
@@ -5,12 +5,26 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableMapUpgradeable.sol";
 
 contract Configuration is OwnableUpgradeable {
+    string private constant TWITTER = "TWITTER";
+    string private constant DISCORD = "DISCORD";
+    string private constant WEBSITE = "WEBSITE";
+    string private constant OTHERS = "OTHERS";
+    string private constant LINKEDIN = "LINKEDIN";
+
     using EnumerableMapUpgradeable for EnumerableMapUpgradeable.UintToUintMap;
     EnumerableMapUpgradeable.UintToUintMap private feeMap;
+    string[] private urlsKeys;
+
+    error UrlKeyAlreadyExist(string key, string message);
 
     function initialize() external initializer {
         __Ownable_init();
         _populateFeeMap();
+        urlsKeys.push(TWITTER);
+        urlsKeys.push(DISCORD);
+        urlsKeys.push(WEBSITE);
+        urlsKeys.push(OTHERS);
+        urlsKeys.push(LINKEDIN);
     }
 
     function setTransactionFee(
@@ -32,6 +46,26 @@ contract Configuration is OwnableUpgradeable {
             feeItems[i * 2] = key;
             feeItems[i * 2 + 1] = value;
         }
+    }
+
+    function addUrlKey(string memory _newKey) public onlyOwner {
+        for (uint i = 0; i < urlsKeys.length; i++) {
+            if (
+                keccak256(abi.encodePacked(urlsKeys[i])) ==
+                keccak256(abi.encodePacked(_newKey))
+            ) {
+                revert UrlKeyAlreadyExist(_newKey, "Url key already exists");
+            }
+        }
+        urlsKeys.push(_newKey);
+    }
+
+    function getCommaSeparatedUrlKeys() public view returns (string memory) {
+        string memory allKeys = "";
+        for (uint i = 0; i < urlsKeys.length; i++) {
+            allKeys = string.concat(urlsKeys[i], ",", allKeys);
+        }
+        return allKeys;
     }
 
     function _populateFeeMap() private {
