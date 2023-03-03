593d0ffe72cc8be59efa3343b69db506de7913a7
shivamgarglab49 <107450337+shivamgarglab49@users.noreply.github.com>
2023-03-03 19:27:36 +0530

    added dev changes (#350)
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    Co-authored-by: shivam_garg <shivam.garg@iongroup.com>

---
+++ deployment/scripts/createGovernanceDAO.ts
@@ -0,0 +1,35 @@
+import { Helper } from "../../utils/Helper";
+import { TokenId, AccountId, ContractId } from "@hashgraph/sdk";
+
+import GovernanceDAOFactory from "../../e2e-test/business/GovernanceDAOFactory";
+
+async function main() {
+  const input = Helper.readWorkflowInputs();
+  if (!input) {
+    throw Error("- Failed to read workflow inputs");
+  }
+
+  // below calls are validating input data only
+  const contractId = ContractId.fromString(input.contractId);
+  TokenId.fromSolidityAddress(input.tokenAddress);
+  AccountId.fromSolidityAddress(input.doaAdmin);
+
+  const governanceDAOFactory = new GovernanceDAOFactory(contractId.toString());
+  await governanceDAOFactory.createDAO(
+    input.doaName,
+    input.daoLogoUrl,
+    input.tokenAddress,
+    Number(input.quorumThreshold),
+    Number(input.votingDelay),
+    Number(input.votingPeriod),
+    input.isPrivate.toLowerCase() === "true",
+    input.doaAdmin
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
