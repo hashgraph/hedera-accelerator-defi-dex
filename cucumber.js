069bac3cede99dacb3f701facd499af048271e6b
IONTRADING\mandeep.singh <mandeep.singh@lab49.com>
2023-03-16 16:52:49 +0530

    parallel execution

--- cucumber.js
+++ cucumber.js
@@ -5,7 +5,25 @@ let common = [
   '--publish',
 ].join(' ');
 
+let testSuite1 = [
+  'e2e-test/features/**/*.feature',
+  '--require-module ts-node/register',
+  '--require e2e-test/step-definitions/**/*.ts',
+  '--publish',
+  '--tags=@TestSuite-1'
+].join(' ');
+
+let testSuite2 = [
+  'e2e-test/features/**/*.feature',
+  '--require-module ts-node/register',
+  '--require e2e-test/step-definitions/**/*.ts',
+  '--publish',
+  '--tags=@TestSuite-2'
+].join(' ');
+
 module.exports = {
   default: common,
+  suiteOne: testSuite1,
+  suiteTwo: testSuite2
   // More profiles can be added if desired
 };
