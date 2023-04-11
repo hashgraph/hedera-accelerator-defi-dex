da70303b6bf2007c2f15bda36c04d9e57bc43903
Shivam Garg <107450337+shivamgarglab49@users.noreply.github.com>
2023-04-11 17:37:05 +0530

    MultiSigDAOFactory and MultiSigDAO contracts with unit testing (#387)
    * initial changes done
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * multisig factory test added
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * updated dao to support generic method
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * added support for token tranfer from gnosis
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * updated sol files and unit test
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * fixed methods name
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * TokenAssociated event added
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * added TranaferToken event
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * fixed typo
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * fixed pr comments
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * fixed argument name
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * fixed unit test case
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    * fixed unit test case
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    
    ---------
    
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    Co-authored-by: shivam_garg <shivam.garg@iongroup.com>

---
+++ contracts/dao/MultisigDAO.sol
@@ -0,0 +1,111 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./BaseDAO.sol";
+import "../common/IBaseHTS.sol";
+import "../gnosis/HederaGnosisSafe.sol";
+import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+
+contract MultiSigDAO is BaseDAO {
+    event TransactionCreated(bytes32 txnHash, TransactionInfo info);
+
+    enum TransactionState {
+        Pending,
+        Approved,
+        Executed
+    }
+
+    struct TransactionInfo {
+        address to;
+        uint256 value;
+        bytes data;
+        Enum.Operation operation;
+        uint256 nonce;
+    }
+
+    // HederaGnosisSafe#transferTokenViaSafe(address token, address receiver, uint256 amount)
+    // 0xbb34db5a - keccack("transferTokenViaSafe(address,address,uint256)")
+    bytes4 private constant TRANSFER_TOKEN_FROM_SAFE_SELECTOR = 0xbb34db5a;
+
+    IBaseHTS private baseHTS;
+    HederaGnosisSafe private hederaGnosisSafe;
+    mapping(bytes32 => TransactionInfo) private transactions;
+
+    function initialize(
+        address _admin,
+        string calldata _name,
+        string calldata _logoUrl,
+        HederaGnosisSafe _hederaGnosisSafe,
+        IBaseHTS _iBaseHTS
+    ) external initializer {
+        baseHTS = _iBaseHTS;
+        hederaGnosisSafe = _hederaGnosisSafe;
+        __BaseDAO_init(_admin, _name, _logoUrl);
+    }
+
+    function getHederaGnosisSafeContractAddress()
+        external
+        view
+        returns (address)
+    {
+        return address(hederaGnosisSafe);
+    }
+
+    function state(bytes32 _txnHash) external view returns (TransactionState) {
+        TransactionInfo memory transactionInfo = transactions[_txnHash];
+        require(transactionInfo.nonce != 0, "MultiSigDAO: no txn exist");
+        if (hederaGnosisSafe.isTransactionExecuted(_txnHash)) {
+            return TransactionState.Executed;
+        } else if (hederaGnosisSafe.checkApprovals(_txnHash)) {
+            return TransactionState.Approved;
+        } else {
+            return TransactionState.Pending;
+        }
+    }
+
+    function getTransactionInfo(
+        bytes32 _txnHash
+    ) external view returns (TransactionInfo memory) {
+        TransactionInfo memory transactionInfo = transactions[_txnHash];
+        require(transactionInfo.nonce != 0, "MultiSigDAO: no txn exist");
+        return transactionInfo;
+    }
+
+    function proposeTransaction(
+        address _to,
+        bytes memory _data,
+        Enum.Operation _operation
+    ) public payable returns (bytes32) {
+        (bytes32 txnHash, uint256 txnNonce) = hederaGnosisSafe.getTxnHash(
+            _to,
+            msg.value,
+            _data,
+            _operation
+        );
+        TransactionInfo storage transactionInfo = transactions[txnHash];
+        transactionInfo.to = _to;
+        transactionInfo.value = msg.value;
+        transactionInfo.data = _data;
+        transactionInfo.operation = _operation;
+        transactionInfo.nonce = txnNonce;
+
+        emit TransactionCreated(txnHash, transactionInfo);
+        return txnHash;
+    }
+
+    function proposeTransferTransaction(
+        address _token,
+        address _receiver,
+        uint256 _amount
+    ) external payable returns (bytes32) {
+        hederaGnosisSafe.transferToSafe(baseHTS, _token, _amount, msg.sender);
+        bytes memory data = abi.encodeWithSelector(
+            TRANSFER_TOKEN_FROM_SAFE_SELECTOR,
+            _token,
+            _receiver,
+            _amount
+        );
+        Enum.Operation call = Enum.Operation.Call;
+        return proposeTransaction(address(hederaGnosisSafe), data, call);
+    }
+}
