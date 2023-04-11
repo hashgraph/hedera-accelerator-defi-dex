0120e10caa26576ff48bb99ea783b7218fde7797
Abhishek Sharma <abhishek.k.sharma@lab49.com>
2023-04-11 11:36:10 +0530

    nft-dao-factory changes are done

---
+++ contracts/dao/DAOFactory.sol
@@ -0,0 +1,202 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "../common/IERC20.sol";
+import "../common/IEvents.sol";
+import "../common/IErrors.sol";
+import "../common/IBaseHTS.sol";
+
+import "../dao/ITokenDAO.sol";
+import "../governance/ITokenHolderFactory.sol";
+import "../governance/IGovernorTransferToken.sol";
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract DAOFactory is OwnableUpgradeable, IEvents, IErrors {
+    event PublicDaoCreated(address daoAddress);
+    event PrivateDaoCreated(address daoAddress);
+
+    error NotAdmin(string message);
+
+    string private constant TokenDAO = "TokenDAO";
+    string private constant TransferToken = "TransferToken";
+    string private constant TokenHolderFactory = "TokenHolderFactory";
+
+    IBaseHTS private baseHTS;
+    address private proxyAdmin;
+
+    address[] private daos;
+
+    ITokenDAO private daoLogic;
+    IGovernorTransferToken private tokenTransferLogic;
+    ITokenHolderFactory private tokenHolderFactory;
+
+    modifier ifAdmin() {
+        if (msg.sender != proxyAdmin) {
+            revert NotAdmin("GovernanceDAOFactory: auth failed");
+        }
+        _;
+    }
+
+    function initialize(
+        address _proxyAdmin,
+        IBaseHTS _baseHTS,
+        ITokenDAO _daoLogic,
+        ITokenHolderFactory _tokenHolderFactory,
+        IGovernorTransferToken _tokenTransferLogic
+    ) external initializer {
+        __Ownable_init();
+        proxyAdmin = _proxyAdmin;
+        baseHTS = _baseHTS;
+        daoLogic = _daoLogic;
+        tokenHolderFactory = _tokenHolderFactory;
+        tokenTransferLogic = _tokenTransferLogic;
+
+        emit LogicUpdated(address(0), address(daoLogic), TokenDAO);
+        emit LogicUpdated(
+            address(0),
+            address(tokenTransferLogic),
+            TransferToken
+        );
+        emit LogicUpdated(
+            address(0),
+            address(tokenHolderFactory),
+            TokenHolderFactory
+        );
+    }
+
+    function getGODTokenHolderFactoryAddress()
+        external
+        view
+        ifAdmin
+        returns (address)
+    {
+        return address(tokenHolderFactory);
+    }
+
+    function upgradeTokenHolderFactory(
+        ITokenHolderFactory _newTokenHolderFactory
+    ) external ifAdmin {
+        emit LogicUpdated(
+            address(tokenHolderFactory),
+            address(_newTokenHolderFactory),
+            TokenHolderFactory
+        );
+        tokenHolderFactory = _newTokenHolderFactory;
+    }
+
+    function upgradeTokenDaoLogicImplementation(
+        ITokenDAO _newImpl
+    ) external ifAdmin {
+        emit LogicUpdated(address(daoLogic), address(_newImpl), TokenDAO);
+        daoLogic = _newImpl;
+    }
+
+    function upgradeTokenTransferLogicImplementation(
+        IGovernorTransferToken _newImpl
+    ) external ifAdmin {
+        emit LogicUpdated(
+            address(tokenTransferLogic),
+            address(_newImpl),
+            TransferToken
+        );
+        tokenTransferLogic = _newImpl;
+    }
+
+    function createDAO(
+        address _admin,
+        string calldata _name,
+        string calldata _logoUrl,
+        IERC20 _tokenAddress,
+        uint256 _quorumThreshold,
+        uint256 _votingDelay,
+        uint256 _votingPeriod,
+        bool _isPrivate
+    ) external returns (address) {
+        if (bytes(_name).length == 0) {
+            revert InvalidInput("GovernanceDAOFactory: name is empty");
+        }
+        if (bytes(_logoUrl).length == 0) {
+            revert InvalidInput("GovernanceDAOFactory: url is empty");
+        }
+        if (address(_tokenAddress) == address(0)) {
+            revert InvalidInput("GovernanceDAOFactory: token address is zero");
+        }
+        if (address(_admin) == address(0)) {
+            revert InvalidInput("GovernanceDAOFactory: admin address is zero");
+        }
+        if (_votingPeriod == 0) {
+            revert InvalidInput("GovernanceDAOFactory: voting period is zero");
+        }
+        ITokenHolder iTokenHolder = tokenHolderFactory.getTokenHolder(
+            address(_tokenAddress)
+        );
+        IGovernorTransferToken tokenTransfer = _createGovernorTransferTokenContractInstance(
+                address(_tokenAddress),
+                _quorumThreshold,
+                _votingDelay,
+                _votingPeriod,
+                iTokenHolder
+            );
+        address createdDAOAddress = _createTokenDAOContractInstance(
+            _admin,
+            _name,
+            _logoUrl,
+            tokenTransfer
+        );
+        if (_isPrivate) {
+            emit PrivateDaoCreated(createdDAOAddress);
+        } else {
+            daos.push(createdDAOAddress);
+            emit PublicDaoCreated(createdDAOAddress);
+        }
+        return createdDAOAddress;
+    }
+
+    function getDAOs() external view returns (address[] memory) {
+        return daos;
+    }
+
+    function _createGovernorTransferTokenContractInstance(
+        address _tokenAddress,
+        uint256 _quorumThreshold,
+        uint256 _votingDelay,
+        uint256 _votingPeriod,
+        ITokenHolder _iTokenHolder
+    ) private returns (IGovernorTransferToken iGovernorTransferToken) {
+        iGovernorTransferToken = IGovernorTransferToken(
+            _createProxy(address(tokenTransferLogic))
+        );
+        iGovernorTransferToken.initialize(
+            IERC20(_tokenAddress),
+            _votingDelay,
+            _votingPeriod,
+            baseHTS,
+            _iTokenHolder,
+            _quorumThreshold
+        );
+    }
+
+    function _createTokenDAOContractInstance(
+        address _admin,
+        string calldata _name,
+        string calldata _logoUrl,
+        IGovernorTransferToken _governorTokenTransferContractAddress
+    ) private returns (address daoAddress) {
+        ITokenDAO tokenDAO = ITokenDAO(_createProxy(address(daoLogic)));
+        tokenDAO.initialize(
+            _admin,
+            _name,
+            _logoUrl,
+            _governorTokenTransferContractAddress
+        );
+        return address(tokenDAO);
+    }
+
+    function _createProxy(address _logic) private returns (address) {
+        bytes memory _data;
+        return
+            address(new TransparentUpgradeableProxy(_logic, proxyAdmin, _data));
+    }
+}
