28ccaa27b9ffccd312a4c6655b744e9e3b258a5e
shivam_garg <shivam.garg@iongroup.com>
2023-05-01 18:04:55 +0530

    attempted to fix

---
+++ deployment/scripts/createContractsE2E1.ts
@@ -0,0 +1,36 @@
+import { Helper } from "../../utils/Helper";
+import { main as deployContract } from "./logic";
+import { main as createProxy } from "./transparentUpgradeableProxy";
+
+console.log("inside file");
+export async function main(contracts: string[]) {
+  try {
+    console.log(process.env.CONTRACTS);
+  } catch (error) {
+    console.log(error);
+  }
+  if (contracts.length === 0) {
+    contracts = String(process.env.CONTRACTS).split(",");
+  }
+  console.log("- Contracts for deployment are:", contracts);
+
+  const startTime = Helper.currentTimeInMills();
+  // await Promise.all(
+  //   contracts.map(async (contractName: string) => {
+  //     await deployContract(contractName);
+  //     await createProxy(contractName);
+  //   })
+  // );
+  console.log(
+    `- Deployment took: ${Helper.currentTimeInMills() - startTime} ms`
+  );
+}
+
+if (require.main === module) {
+  main([])
+    .then((env) => {
+      console.log(env);
+      process.exit(0);
+    })
+    .catch(Helper.processError);
+}
