c75ce095609f0cfb872991bbc8859497b23a72f7
shivam_garg <shivam.garg@iongroup.com>
2023-05-02 12:35:35 +0530

    new file

--- deployment/scripts/createContractsE2E.ts
+++ deployment/scripts/createContractsE2E.ts
@@ -1,22 +1,13 @@
-import hre from "hardhat";
-
 import { Helper } from "../../utils/Helper";
 import { main as deployContract } from "./logic";
 import { main as createProxy } from "./transparentUpgradeableProxy";
 
 export async function main(contracts: string[]) {
-  let delay = 0;
   if (contracts.length === 0) {
     const inputs = Helper.readWorkflowInputs();
-    delay = inputs.delay;
     contracts = String(inputs.contracts).split(",");
   }
   console.log("- Contracts for deployment are:", contracts);
-  if (delay > 0) {
-    console.log(`- Contracts compilation delayed by: ${delay} ms`);
-    await Helper.delay(delay);
-  }
-  await hre.run("compile");
   const startTime = Helper.currentTimeInMills();
   await Promise.all(
     contracts.map(async (contractName: string) => {
