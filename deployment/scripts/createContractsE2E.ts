32e90b4d1d44e3b5e30ba3948c84329e72ce1e0d
shivam_garg <shivam.garg@iongroup.com>
2023-05-02 12:11:23 +0530

    attempted to fix
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>

--- deployment/scripts/createContractsE2E.ts
+++ deployment/scripts/createContractsE2E.ts
@@ -1,33 +1,29 @@
+import hre from "hardhat";
+
 import { Helper } from "../../utils/Helper";
 import { main as deployContract } from "./logic";
 import { main as createProxy } from "./transparentUpgradeableProxy";
-import hre from "hardhat";
 
-console.log("inside file");
 export async function main(contracts: string[]) {
-  try {
-    console.log(process.env.CONTRACTS);
-  } catch (error) {
-    console.log(error);
-  }
+  let delayRequired = false;
   if (contracts.length === 0) {
-    contracts = String(process.env.CONTRACTS).split(",");
+    const inputs = Helper.readWorkflowInputs();
+    delayRequired = inputs.delayRequired;
+    contracts = String(inputs.contracts).split(",");
   }
   console.log("- Contracts for deployment are:", contracts);
-
-  if (process.env.WAIT === "true") {
-    console.log("- waiting");
-    await Helper.delay(40000);
+  if (delayRequired) {
+    console.log("- Contracts compilation delayed");
+    await Helper.delay(20e3);
   }
   await hre.run("compile");
-
   const startTime = Helper.currentTimeInMills();
-  // await Promise.all(
-  //   contracts.map(async (contractName: string) => {
-  //     await deployContract(contractName);
-  //     await createProxy(contractName);
-  //   })
-  // );
+  await Promise.all(
+    contracts.map(async (contractName: string) => {
+      await deployContract(contractName);
+      await createProxy(contractName);
+    })
+  );
   console.log(
     `- Deployment took: ${Helper.currentTimeInMills() - startTime} ms`
   );
