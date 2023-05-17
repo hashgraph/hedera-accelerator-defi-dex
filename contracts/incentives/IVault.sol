3b7fb39f35688ebadde8c214e6a23f8f55fab5a4
shivam_garg <shivam.garg@iongroup.com>
2023-05-17 17:16:13 +0530

    fixed vault v36
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>

--- contracts/incentives/IVault.sol
+++ contracts/incentives/IVault.sol
@@ -2,16 +2,35 @@
 
 pragma solidity ^0.8.18;
 
+import "../common/IBaseHTS.sol";
+
 interface IVault {
+    function initialize(
+        IBaseHTS _baseHTS,
+        address _stakingToken,
+        uint256 _lockingPeriod
+    ) external;
+
     function addReward(
         address _token,
         uint256 _amount,
         address _fromAccount
     ) external;
 
-    function addStake(uint256 _amount) external returns (uint256 timeStamp);
+    function deposit(uint256 _amount) external;
 
-    function getTotalVolume() external view returns (uint);
+    function withdraw(uint256 _amount) external;
 
-    function getLockPeriod() external view returns (uint);
+    function getTotalVolume() external view returns (uint256);
+
+    function getLockingPeriod() external view returns (uint256);
+
+    function getUserContribution(address _user) external view returns (uint256);
+
+    function claimAllRewards(address _user) external;
+
+    function claimSpecificRewards(
+        address _user,
+        address[] memory _rewardTokens
+    ) external;
 }
--- contracts/incentives/Vault.sol
+++ contracts/incentives/Vault.sol
@@ -1,205 +1,203 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "../common/IBaseHTS.sol";
+import "./IVault.sol";
 import "../common/IERC20.sol";
-import "../common/hedera/HederaTokenService.sol";
-import "prb-math/contracts/PRBMathUD60x18.sol";
+import "../common/IErrors.sol";
+import "../common/TokenOperations.sol";
 import "../common/hedera/HederaResponseCodes.sol";
+import "prb-math/contracts/PRBMathUD60x18.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Vault is Initializable {
-    struct UserInfo {
-        uint256 shares;
-        mapping(address => uint256) lastClaimedAmount;
-        uint256 lockTimeStart;
-        bool exist;
-    }
-
-    struct RewardsInfo {
-        uint256 amount;
-        bool exist;
-    }
-
+contract Vault is IVault, OwnableUpgradeable, TokenOperations {
     using PRBMathUD60x18 for uint256;
+
+    struct ContributionInfo {
+        bool exist;
+        uint256 total;
+        uint256 lockedTime;
+        mapping(address => uint256) claimed;
+    }
+
+    struct RewardInfo {
+        bool exist;
+        uint256 perShareAmount;
+    }
+
+    IBaseHTS private baseHTS;
     IERC20 private stakingToken;
-    uint256 private lockPeriod;
-    uint256 private stakingTokenTotalAmount;
+
+    uint256 private totalSupply;
+    uint256 private lockingPeriod;
+
+    mapping(address => ContributionInfo) public usersContributionInfo;
+
     address[] private rewardTokens;
-    address private owner;
-    IBaseHTS private tokenService;
-    mapping(address => UserInfo) public userStakedTokenContribution;
-    mapping(address => RewardsInfo) public rewards;
+    mapping(address => RewardInfo) public tokensRewardInfo;
 
     function initialize(
+        IBaseHTS _baseHTS,
         address _stakingToken,
-        uint256 _lockPeriod,
-        IBaseHTS _tokenService
+        uint256 _lockingPeriod
     ) public initializer {
+        __Ownable_init();
         require(
             _stakingToken != address(0),
-            "Staking token should not be empty."
+            "Vault: Staking token should not be empty."
         );
-        lockPeriod = _lockPeriod;
-        tokenService = _tokenService;
-        tokenService.associateTokenPublic(address(this), _stakingToken);
+        require(
+            _lockingPeriod > 0,
+            "Vault: locking period should be a postive number"
+        );
+        baseHTS = _baseHTS;
+        lockingPeriod = _lockingPeriod;
         stakingToken = IERC20(_stakingToken);
+        _associateToken(_baseHTS, address(this), _stakingToken);
     }
 
-    //we need to set the amount of each reward address to the lastClaimed amount of the user
-    function addStake(uint256 amount) external returns (uint256 timeStamp) {
-        require(amount != 0, "Please provide amount");
-        if (!userStakedTokenContribution[msg.sender].exist) {
-            _setUpStaker(msg.sender);
-            return _updateStakeContribution(msg.sender, amount);
+    function deposit(uint256 _amount) external override {
+        require(_amount > 0, "Vault: deposit amount must be a postive number");
+        if (usersContributionInfo[msg.sender].exist) {
+            claimAllRewards(msg.sender);
         } else {
-            claimAllReward(0, msg.sender);
-            return _updateStakeContribution(msg.sender, amount);
+            _setUpStaker(msg.sender);
         }
+        _deposit(msg.sender, _amount);
+    }
+
+    function withdraw(uint256 _amount) external override {
+        require(_amount > 0, "Vault: withdraw amount must be a postive number");
+        _validateLockingPeriod(msg.sender);
+        claimAllRewards(msg.sender);
     }
 
     function addReward(
         address _token,
         uint256 _amount,
-        address _fromAccount
-    ) external {
-        require(_amount != 0, "Please provide amount");
-        require(stakingTokenTotalAmount != 0, "No token staked yet");
-        uint256 perShareRewards;
-        perShareRewards = _amount.div(stakingTokenTotalAmount);
-        if (!rewards[_token].exist) {
+        address _from
+    ) external override onlyOwner {
+        require(_token != address(0), "Vault: reward token address is zero");
+        require(_from != address(0), "Vault: from address is zero");
+        require(_amount > 0, "Vault: reward amount must be a positive number");
+        require(totalSupply > 0, "Vault: no token staked yet");
+        uint256 perShareAmount = _amount.div(totalSupply);
+        RewardInfo storage rewardInfo = tokensRewardInfo[_token];
+        if (!rewardInfo.exist) {
+            rewardInfo.exist = true;
             rewardTokens.push(_token);
-            rewards[_token].exist = true;
-            rewards[_token].amount = perShareRewards;
-            tokenService.associateTokenPublic(address(this), _token);
-            int256 responseCode = tokenService.transferTokenPublic(
-                address(_token),
-                address(_fromAccount),
-                address(this),
-                int64(uint64(_amount))
-            );
-            require(
-                responseCode == HederaResponseCodes.SUCCESS,
-                "Vault: Add reward failed on token exist."
-            );
-        } else {
-            rewards[_token].amount += perShareRewards;
-            int256 responseCode = tokenService.transferTokenPublic(
-                address(_token),
-                address(_fromAccount),
-                address(this),
-                int64(uint64(_amount))
-            );
-            require(
-                responseCode == HederaResponseCodes.SUCCESS,
-                "Vault: Add reward failed on token not exist."
-            );
+            _associateToken(baseHTS, address(this), _token);
         }
-    }
-
-    function withdraw(uint256 startPosition, uint256 amount) public {
-        require(amount != 0, "Please provide amount");
-        _unlock(msg.sender);
-        claimAllReward(startPosition, (msg.sender));
-        bool isTransferSuccessful = stakingToken.transfer(msg.sender, amount);
-        require(isTransferSuccessful, "Vault: Withdraw failed.");
-
-        userStakedTokenContribution[msg.sender].shares -= amount;
-        stakingTokenTotalAmount -= amount;
-    }
-
-    function _setUpStaker(address user) private {
-        for (uint256 i; i < rewardTokens.length; i++) {
-            address token = rewardTokens[i];
-            userStakedTokenContribution[user].lastClaimedAmount[
-                token
-            ] = rewards[token].amount;
-            tokenService.associateTokenPublic(address(user), token);
-        }
-    }
-
-    function _updateStakeContribution(
-        address user,
-        uint256 amount
-    ) private returns (uint256 timeStamp) {
-        tokenService.associateTokenPublic(address(this), address(stakingToken));
-        int256 responseCode = tokenService.transferTokenPublic(
-            address(stakingToken),
-            user,
-            address(this),
-            int64(uint64(amount))
-        );
+        rewardInfo.perShareAmount += perShareAmount;
         require(
-            responseCode == HederaResponseCodes.SUCCESS,
+            _transferToken(_token, _from, address(this), int256(_amount)) ==
+                HederaResponseCodes.SUCCESS,
+            "Vault: Add reward failed."
+        );
+    }
+
+    function getUserContribution(
+        address _user
+    ) external view override returns (uint256) {
+        return usersContributionInfo[_user].total;
+    }
+
+    function claimAllRewards(address _user) public override {
+        this.claimSpecificRewards(_user, rewardTokens);
+    }
+
+    function claimSpecificRewards(
+        address _user,
+        address[] memory _rewardTokens
+    ) public override {
+        for (uint256 i = 0; i < _rewardTokens.length; i++) {
+            address rewardToken = _rewardTokens[i];
+            RewardInfo memory rewardInfo = tokensRewardInfo[rewardToken];
+            if (rewardInfo.exist) {
+                _claimReward(_user, rewardToken, rewardInfo);
+            } else {
+                revert("Vault: invalid token");
+            }
+        }
+    }
+
+    function getTotalVolume() external view override returns (uint256) {
+        return totalSupply;
+    }
+
+    function getLockingPeriod() external view override returns (uint256) {
+        return lockingPeriod;
+    }
+
+    function _claimReward(
+        address _user,
+        address _rewardToken,
+        RewardInfo memory _rewardInfo
+    ) private {
+        uint256 perShareRewardAmt = _rewardInfo.perShareAmount;
+        ContributionInfo storage cInfo = usersContributionInfo[_user];
+        uint256 totalShares = cInfo.total;
+        uint256 perShareClaimedAmt = cInfo.claimed[_rewardToken];
+        uint256 perShareUnclaimedAmt = perShareRewardAmt - perShareClaimedAmt;
+        uint256 unclaimedRewards = totalShares.mul(perShareUnclaimedAmt);
+        cInfo.claimed[_rewardToken] = perShareRewardAmt;
+        require(
+            _transferToken(
+                _rewardToken,
+                address(this),
+                _user,
+                int256(unclaimedRewards)
+            ) == HederaResponseCodes.SUCCESS,
+            "Vault: Claim reward failed."
+        );
+    }
+
+    function _deposit(address _user, uint256 _amount) private {
+        ContributionInfo storage cInfo = usersContributionInfo[_user];
+        cInfo.exist = true;
+        cInfo.lockedTime = block.timestamp;
+        cInfo.total += _amount;
+        totalSupply += _amount;
+        require(
+            _transferToken(
+                address(stakingToken),
+                _user,
+                address(this),
+                int256(_amount)
+            ) == HederaResponseCodes.SUCCESS,
             "Vault: Add stake failed."
         );
-        userStakedTokenContribution[user].shares += amount;
-        userStakedTokenContribution[user].exist = true;
-        userStakedTokenContribution[user].lockTimeStart = block.timestamp;
-        stakingTokenTotalAmount += amount;
-        return block.timestamp;
     }
 
-    function _unlock(address user) private view {
+    function _withdraw(address _user, uint256 _amount) private {
+        ContributionInfo storage cInfo = usersContributionInfo[_user];
+        cInfo.total -= _amount;
+        totalSupply -= _amount;
         require(
-            (userStakedTokenContribution[user].lockTimeStart + lockPeriod) <
-                block.timestamp,
-            "you can't unlock your token because the lock period is not reached"
+            _transferToken(
+                address(stakingToken),
+                address(this),
+                _user,
+                int256(_amount)
+            ) == HederaResponseCodes.SUCCESS,
+            "Vault: withdraw failed."
         );
     }
 
-    function claimAllReward(
-        uint256 startPosition,
-        address user
-    ) public returns (uint256, uint256) {
-        for (
-            uint256 i = startPosition;
-            i < rewardTokens.length && i < startPosition + 10;
-            i++
-        ) {
-            address token = rewardTokens[i];
-            _claimRewardForToken(token, user);
+    function _validateLockingPeriod(address _user) private view {
+        ContributionInfo storage cInfo = usersContributionInfo[_user];
+        require(
+            cInfo.lockedTime + lockingPeriod < block.timestamp,
+            "Vault: you can't unlock your tokens because the locking period is not over"
+        );
+    }
+
+    function _setUpStaker(address _user) private {
+        ContributionInfo storage cInfo = usersContributionInfo[_user];
+        uint256 rewardTokensCount = rewardTokens.length;
+        for (uint256 counter = 0; counter < rewardTokensCount; counter++) {
+            address rewardToken = rewardTokens[counter];
+            cInfo.claimed[rewardToken] = tokensRewardInfo[rewardToken]
+                .perShareAmount;
         }
-        return (startPosition, rewardTokens.length);
-    }
-
-    function claimSpecificReward(
-        address[] memory tokens,
-        address user
-    ) public returns (uint256) {
-        for (uint256 i; i < tokens.length; i++) {
-            address token = tokens[i];
-            _claimRewardForToken(token, user);
-        }
-        return tokens.length;
-    }
-
-    function _claimRewardForToken(address token, address user) private {
-        uint256 reward;
-        if (userStakedTokenContribution[user].lastClaimedAmount[token] == 0) {
-            tokenService.associateTokenPublic(address(user), token);
-        }
-        reward = (rewards[token].amount -
-            userStakedTokenContribution[user].lastClaimedAmount[token]).mul(
-                userStakedTokenContribution[user].shares
-            );
-        userStakedTokenContribution[user].lastClaimedAmount[token] = rewards[
-            token
-        ].amount;
-
-        bool isTransferSuccessful = IERC20(token).transfer(user, reward);
-        require(isTransferSuccessful, "Vault: Claim reward failed.");
-    }
-
-    function getLockedAmount(address user) public view returns (uint256) {
-        return userStakedTokenContribution[user].shares;
-    }
-
-    function getTotalVolume() public view returns (uint256) {
-        return stakingTokenTotalAmount;
-    }
-
-    function getLockPeriod() public view returns (uint256) {
-        return lockPeriod;
     }
 }
