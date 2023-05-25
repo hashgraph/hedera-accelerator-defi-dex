497ba0399b5d1139c8bfa6f946654c6e69c8510a
Shivam Garg <107450337+shivamgarglab49@users.noreply.github.com>
2023-05-25 16:43:35 +0530

    initial changes (#499)
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>
    Co-authored-by: shivam_garg <shivam.garg@iongroup.com>

--- e2e-test/business/Base.ts
+++ e2e-test/business/Base.ts
@@ -64,7 +64,7 @@ export default class Base {
     };
   };
 
-  protected isInitializationPending = async () => {
+  public isInitializationPending = async () => {
     return await MirrorNodeService.getInstance().isInitializationPending(
       this.contractId
     );
---
+++ integrationTest/incentives/vault.ts
@@ -0,0 +1,82 @@
+import dex from "../../deployment/model/dex";
+import Vault from "../../e2e-test/business/Vault";
+import Common from "../../e2e-test/business/Common";
+
+import { Helper } from "../../utils/Helper";
+import { TokenId } from "@hashgraph/sdk";
+import { clientsInfo } from "../../utils/ClientManagement";
+import { InstanceProvider } from "../../utils/InstanceProvider";
+
+const LOCKING_PERIOD_IN_SECONDS = 15; // 15 second locking period
+
+const STAKING_TOKEN = TokenId.fromString(dex.GOD_TOKEN_ID);
+const STAKING_TOKEN_QTY = Common.withPrecision(1, 1).toNumber();
+
+const REWARD_TOKEN = TokenId.fromString(dex.TOKEN_LAB49_1);
+const REWARD_TOKEN_1 = TokenId.fromString(dex.TOKEN_LAB49_2);
+const REWARD_TOKEN_QTY = Common.withPrecision(1, 1).toNumber();
+
+const addRewards = async (vault: Vault, rToken: TokenId) => {
+  await Common.setTokenAllowance(
+    rToken,
+    vault.contractId,
+    REWARD_TOKEN_QTY,
+    clientsInfo.operatorId,
+    clientsInfo.operatorKey,
+    clientsInfo.operatorClient
+  );
+  await vault.addReward(
+    rToken,
+    REWARD_TOKEN_QTY,
+    clientsInfo.operatorId,
+    clientsInfo.operatorClient
+  );
+};
+
+const stake = async (vault: Vault) => {
+  await Common.setTokenAllowance(
+    STAKING_TOKEN,
+    vault.contractId,
+    STAKING_TOKEN_QTY,
+    clientsInfo.uiUserId,
+    clientsInfo.uiUserKey,
+    clientsInfo.uiUserClient
+  );
+  await vault.stake(STAKING_TOKEN_QTY, clientsInfo.uiUserClient);
+};
+
+async function main() {
+  const vault = InstanceProvider.getInstance().getVault();
+  await vault.initialize(STAKING_TOKEN, LOCKING_PERIOD_IN_SECONDS);
+  await vault.getStakingTokenAddress();
+  await vault.getStakingTokenLockingPeriod();
+
+  // user who has staked their token must associate rewards token so they can earn rewards
+  await Common.associateTokensToAccount(
+    clientsInfo.uiUserId,
+    [REWARD_TOKEN, REWARD_TOKEN_1],
+    clientsInfo.uiUserClient,
+    clientsInfo.uiUserKey
+  );
+
+  await stake(vault);
+  await addRewards(vault, REWARD_TOKEN);
+  await addRewards(vault, REWARD_TOKEN_1);
+  await vault.getStakingTokenTotalSupply();
+
+  const amount = await vault.stakedTokenByUser(clientsInfo.uiUserId);
+  await vault.canUserUnStakeTokens(clientsInfo.uiUserId, amount);
+  await Helper.delay(LOCKING_PERIOD_IN_SECONDS * 1e3);
+  await vault.canUserUnStakeTokens(clientsInfo.uiUserId, amount);
+  const rewardsAvailable = await vault.canUserClaimRewards(
+    clientsInfo.uiUserId
+  );
+  rewardsAvailable && (await vault.claimRewards(clientsInfo.uiUserId));
+  await vault.canUserUnStakeTokens(clientsInfo.uiUserId, amount);
+  await vault.unstake(amount, clientsInfo.uiUserClient);
+  await vault.getStakingTokenTotalSupply();
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(Helper.processError);
