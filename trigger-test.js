d7ac5893b73778da7382956b32663f6af31f9849
shivam_garg <shivam.garg@iongroup.com>
2023-07-27 17:33:22 +0530

    attempted to fix e2e exit code
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>

--- trigger-test.js
+++ trigger-test.js
@@ -1,35 +1,37 @@
-const {
-  spawn
-} = require('child_process')
-
-var args = process.argv.slice(2);
+const { spawn } = require("child_process");
+const E2E_FAILED_CODE_REGEX = /(\d+) steps \((\d+) failed\.*/;
+const args = process.argv.slice(2);
 // your cmd to start the process, possibly spawn('npm.cmd', ['test')) for win
 
 const suiteName = args[0];
-const proc = spawn('npm',['run', suiteName]) // change it to npm for linux
+const proc = spawn("npm", ["run", suiteName]); // change it to npm for linux
 let flag = false;
-proc.stdout.on('data', (data) => {
-  if(data.includes('failed')){
+proc.stdout.on("data", (data) => {
+  if (E2E_FAILED_CODE_REGEX.exec(data)) {
     flag = true;
   }
-  console.log(`stdout: ${data}`)
-  if (data.includes('View your Cucumber Report at')) {
-    console.log('test run completed')
-    setTimeout(() => {process.exit(0);}, 5000);        
+  console.log(`stdout: ${data}`);
+  if (data.includes("View your Cucumber Report at")) {
+    console.log("test run completed");
+    setTimeout(() => {
+      process.exit(0);
+    }, 5000);
   }
-  
-})
+});
 
-proc.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`)
-  if (data.includes('View your Cucumber Report at')) {
-    if(!flag){
-    console.log('test run completed')
-    setTimeout(() => {process.exit(0);}, 5000);   
+proc.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+  if (data.includes("View your Cucumber Report at")) {
+    if (!flag) {
+      console.log("test run completed");
+      setTimeout(() => {
+        process.exit(0);
+      }, 5000);
+    } else {
+      console.log("few test cases failed");
+      setTimeout(() => {
+        process.exit(1);
+      }, 5000);
     }
-    else{
-      console.log('few test cases failed')
-      setTimeout(() => {process.exit(1);}, 5000);
-    }     
   }
 });
