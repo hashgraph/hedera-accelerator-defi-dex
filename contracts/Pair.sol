6b4360ca83d17ba6b6d829dc36929fccd37c89d0
Amandeep Singh (Lab49) <amandeep.rehal@iongroup.com>
2023-08-25 21:38:47 +0530

    Fixed bug

--- contracts/Pair.sol
+++ contracts/Pair.sol
@@ -95,21 +95,57 @@ contract Pair is
 
         uint256 _tokenAQtyBeforeAdding = pair.tokenA.tokenQty;
         uint256 _tokenBQtyBeforeAdding = pair.tokenB.tokenQty;
-        uint256 _incomingTokenAQty;
-        uint256 _incomingTokenBQty;
 
         if (
             _tokenA == pair.tokenA.tokenAddress &&
             _tokenB == pair.tokenB.tokenAddress
         ) {
-            _incomingTokenAQty = _tokenAQty;
-            _incomingTokenBQty = _tokenBQty;
+            pair.tokenA.tokenQty += _tokenAQty;
+            pair.tokenB.tokenQty += _tokenBQty;
+
+            transferTokensInternally(
+                fromAccount,
+                address(this),
+                _tokenA,
+                _tokenB,
+                _tokenAQty,
+                _tokenBQty,
+                "Add liquidity: Transfering token A to contract failed with status code",
+                "Add liquidity: Transfering token B to contract failed with status code"
+            );
+
+            lpTokenContract.allotLPTokenFor(
+                _tokenAQtyBeforeAdding,
+                _tokenBQtyBeforeAdding,
+                _tokenAQty,
+                _tokenBQty,
+                fromAccount
+            );
         } else if (
             _tokenA == pair.tokenB.tokenAddress &&
             _tokenB == pair.tokenA.tokenAddress
         ) {
-            _incomingTokenBQty = _tokenAQty;
-            _incomingTokenAQty = _tokenBQty;
+            pair.tokenA.tokenQty += _tokenBQty;
+            pair.tokenB.tokenQty += _tokenAQty;
+
+            transferTokensInternally(
+                fromAccount,
+                address(this),
+                _tokenB,
+                _tokenA,
+                _tokenBQty,
+                _tokenAQty,
+                "Add liquidity: Transfering token A to contract failed with status code",
+                "Add liquidity: Transfering token B to contract failed with status code"
+            );
+
+            lpTokenContract.allotLPTokenFor(
+                _tokenAQtyBeforeAdding,
+                _tokenBQtyBeforeAdding,
+                _tokenBQty,
+                _tokenAQty,
+                fromAccount
+            );
         } else {
             revert WrongPairPassed({
                 message: "Wrong token pair passed",
@@ -119,28 +155,6 @@ contract Pair is
                 expectedTokenB: pair.tokenB.tokenAddress
             });
         }
-
-        pair.tokenA.tokenQty += _incomingTokenAQty;
-        pair.tokenB.tokenQty += _incomingTokenBQty;
-
-        transferTokensInternally(
-            fromAccount,
-            address(this),
-            _tokenA,
-            _tokenB,
-            _tokenAQty,
-            _tokenBQty,
-            "Add liquidity: Transfering token A to contract failed with status code",
-            "Add liquidity: Transfering token B to contract failed with status code"
-        );
-
-        lpTokenContract.allotLPTokenFor(
-            _tokenAQtyBeforeAdding,
-            _tokenBQtyBeforeAdding,
-            _incomingTokenAQty,
-            _incomingTokenBQty,
-            fromAccount
-        );
     }
 
     function removeLiquidity(
