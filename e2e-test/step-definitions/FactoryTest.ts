6d71ba88b5c8faef026a0eff704cd92cde44d334
Amandeep Singh (Lab49) <amandeep.rehal@iongroup.com>
2023-09-14 16:28:23 +0530

    Reverted file

--- e2e-test/step-definitions/FactoryTest.ts
+++ e2e-test/step-definitions/FactoryTest.ts
@@ -520,9 +520,7 @@ export class FactorySteps {
         fees
       );
     } catch (e: any) {
-      console.error(`e.message ${e.message} `);
       errorMsg = e.message;
-      console.error(`errorMsg ${errorMsg} `);
     }
   }
 
@@ -558,8 +556,6 @@ export class FactorySteps {
 
   @then(/User receive error message "([^"]*)"/, undefined, 30000)
   public async verifyErrorMsg(msg: string) {
-    console.log(`receive error message ${msg}`);
-    console.log(`errorMsg receive error message ${errorMsg}`);
     expect(errorMsg).contains(msg);
     errorMsg = "";
   }
