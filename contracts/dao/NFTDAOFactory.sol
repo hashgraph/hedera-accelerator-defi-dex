53e5b2f2944fde8424089b449f462e1fb929ee18
Roshan Bisht <roshan.bisht@lab49.com>
2023-10-16 13:22:44 +0530

    Added Condition to check FT DAO is created with FT and NFT DAO with NFT
    Signed-off-by: Roshan Bisht <roshan.bisht@lab49.com>

--- contracts/dao/NFTDAOFactory.sol
+++ contracts/dao/NFTDAOFactory.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.18;
 
 import "./FTDAOFactory.sol";
 
-contract NFTDAOFactory is FTDAOFactory {}
+contract NFTDAOFactory is FTDAOFactory {
+    function _isNFTDAOInstance() internal virtual override pure returns(bool) {
+        return true;
+    } 
+}
